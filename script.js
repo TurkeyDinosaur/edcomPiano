77d4699435b63030aba525fc76a325ef105f3578
TurkeyDinosaur <jonnyhale99@gmail.com>
2021-02-14 21:17:15 +0000

    piano chart test

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const synth = new Tone.Synth().toDestination();
-
+//const synth = new Tone.Synth().toDestination();
 //play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "8n");
+//synth.triggerAttackRelease("C4", "8n");
+import { Instrument } from "piano-chart";
+
+const piano = new Instrument(document.getElementById('pianoContainer'));
+piano.create();
+
+piano.keyDown("D4");
+piano.keyDown("F#4");
+piano.keyDown("A4");
